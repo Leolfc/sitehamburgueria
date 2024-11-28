c5d7b4ea63081b384b55b2ab8ada70909711f3e4
Leolfc <147149946+Leolfc@users.noreply.github.com>
2024-11-28 13:23:03 -0300

    atualizado input e estilização

---
+++ script.js/index.js
@@ -0,0 +1,7 @@
+let valorDoLanche = document.querySelectorAll('.valor')
+let car = document.querySelector('#total-container')
+valorDoLanche.forEach((elemento)=>{
+elemento.addEventListener('click', ()=>{
+  
+})
+})
